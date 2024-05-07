5cb613dc168536986d23f03ec9d20930c3800938
李欣瑞 <lowel70501@gmail.com>
2024-05-07 13:04:38 +0800

    -- 不知道為什麼蓋到了,現已修正

--- team6/src/main/java/com/team6/member/model/MemberAccountBean.java
+++ team6/src/main/java/com/team6/member/model/MemberAccountBean.java
@@ -1,10 +1,6 @@
 package com.team6.member.model;
 
-import java.util.List;
-
 import org.springframework.stereotype.Component;
-
-import com.team6.reservation.model.Reserve;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
@@ -12,7 +8,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
@@ -20,106 +15,96 @@ import jakarta.persistence.Table;
 @Component
 public class MemberAccountBean {
 
-	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "maid")
-	private int maid;
-	@Column(name = "maccount")
-	private String mAccount; 
-	@Column(name = "mpassword")
-	private String mPassword; 
-	@Column(name = "permissions")
-	private int permissions;
-	
-	@OneToOne(mappedBy = "accountBean",cascade = CascadeType.ALL)
-	private MemberDetailBean detailBean;
-	
-//	//測試用
-//	@OneToMany(mappedBy = "memberAccountBean",cascade = {CascadeType.REFRESH,CascadeType.MERGE})
-//	private List<Reserve> reserve;
-//	
-//	public List<Reserve> getReserve() {
-//		return reserve;
-//	}
-//	public void setReserve(List<Reserve> reserve) {
-//		this.reserve = reserve;
-//	}	
-//	
-//	public MemberAccountBean(int maid, String mAccount, String mPassword, int permissions, int hidden,
-//			MemberDetailBean detailBean, List<Reserve> reserve) {
-//		this.maid = maid;
-//		this.mAccount = mAccount;
-//		this.mPassword = mPassword;
-//		this.permissions = permissions;
-//		this.hidden = hidden;
-//		this.detailBean = detailBean;
-//		this.reserve = reserve;
-//	}
-//	//測試用
-	
-	// 方法----------------------------------------------------------------
-	public MemberAccountBean() {}
-	public MemberAccountBean(int maid, String mAccount, String mPassword, int permissions,
-			MemberDetailBean detailBean) {
-		this.maid = maid;
-		this.mAccount = mAccount;
-		this.mPassword = mPassword;
-		this.permissions = permissions;
-		this.detailBean = detailBean;
-	}
-	public String saveToCsv() {
-		String csv = maid +","+ mAccount+","+mPassword+","+permissions;
-		return csv;
-	}
-	public String saveToXml() {
-//		String xml="<?xml version=\"1.0\" encoding=\"UTF-8\"?>"+"\n"+"<Root>"+"\n";
-		String xml="<Row>\n"
-				   +"<maid>"+maid+"</maid>\n"
-				   +"<mAccount>"+mAccount+"</mAccount>\n"
-				   +"<mPassword>"+mPassword+"</mPassword>\n"
-				   +"<permissions>"+permissions+"</permissions>\n"
-				   +"</Row>\n";
-		return xml;
-	}
-	public String saveToJson() {
-//		String json="[";
-		String json="{\n\"maid\":\""+maid+"\",\n"
-					+"\"mAccount\":\""+mAccount+"\",\n"
-					+"\"mPassword\":\""+mPassword+"\",\n"
-					+"\"permissions\":\""+permissions+"\"\n},";
-//		json=json.substring(1, json.length()-1)+"\n]";
-		return json;
-	}
+ @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+ @Column(name = "maid")
+ private int maid;
+ @Column(name = "maccount")
+ private String mAccount; 
+ @Column(name = "mpassword")
+ private String mPassword; 
+ @Column(name = "permissions")
+ private int permissions;
+ @Column(name = "hidden")
+ private int hidden;
+// private int i;
+ 
+ @OneToOne(mappedBy = "accountBean",cascade = CascadeType.ALL)
+ private MemberDetailBean detailBean;
+ 
+ // 方法----------------------------------------------------------------
+ public MemberAccountBean() {}
+ public MemberAccountBean(int maid, String mAccount, String mPassword, int permissions, int hidden,
+   MemberDetailBean detailBean) {
+  this.maid = maid;
+  this.mAccount = mAccount;
+  this.mPassword = mPassword;
+  this.permissions = permissions;
+  this.hidden = hidden;
+  this.detailBean = detailBean;
+ }
 
-	// set and get----------------------------------------------------------------
-	public int getMaid() {
-		return maid;
-	}
-	public void setMaid(int maid) {
-		this.maid = maid;
-	}
-	public String getmAccount() {
-		return mAccount;
-	}
-	public void setmAccount(String mAccount) {
-		this.mAccount = mAccount;
-	}
-	public String getmPassword() {
-		return mPassword;
-	}
-	public void setmPassword(String mPassword) {
-		this.mPassword = mPassword;
-	}
-	public MemberDetailBean getDetailBean() {
-		return detailBean;
-	}
-	public void setDetailBean(MemberDetailBean detailBean) {
-		this.detailBean = detailBean;
-	}
-	public int getPermissions() {
-		return permissions;
-	}
-	public void setPermissions(int permissions) {
-		this.permissions = permissions;
-	}
-	
+ public String saveToCsv() {
+  String csv = maid +","+ mAccount+","+mPassword+","+permissions+","+hidden;
+  return csv;
+ }
+ public String saveToXml() {
+//  String xml="<?xml version=\"1.0\" encoding=\"UTF-8\"?>"+"\n"+"<Root>"+"\n";
+  String xml="<Row>\n"
+       +"<maid>"+maid+"</maid>\n"
+       +"<mAccount>"+mAccount+"</mAccount>\n"
+       +"<mPassword>"+mPassword+"</mPassword>\n"
+       +"<permissions>"+permissions+"</permissions>\n"
+       +"<hidden>"+hidden+"</hidden>\n"
+       +"</Row>\n";
+  return xml;
+ }
+ public String saveToJson() {
+//  String json="[";
+  String json="{\n\"maid\":\""+maid+"\",\n"
+     +"\"mAccount\":\""+mAccount+"\",\n"
+     +"\"mPassword\":\""+mPassword+"\",\n"
+     +"\"permissions\":\""+permissions+"\",\n"
+     +"\"hidden\":\""+hidden+"\"\n},";
+//  json=json.substring(1, json.length()-1)+"\n]";
+  return json;
+ }
+
+ // set and get----------------------------------------------------------------
+ public int getMaid() {
+  return maid;
+ }
+ public void setMaid(int maid) {
+  this.maid = maid;
+ }
+ public String getmAccount() {
+  return mAccount;
+ }
+ public void setmAccount(String mAccount) {
+  this.mAccount = mAccount;
+ }
+ public String getmPassword() {
+  return mPassword;
+ }
+ public void setmPassword(String mPassword) {
+  this.mPassword = mPassword;
+ }
+ public MemberDetailBean getDetailBean() {
+  return detailBean;
+ }
+ public void setDetailBean(MemberDetailBean detailBean) {
+  this.detailBean = detailBean;
+ }
+ public int getPermissions() {
+  return permissions;
+ }
+ public void setPermissions(int permissions) {
+  this.permissions = permissions;
+ }
+ public int getHidden() {
+  return hidden;
+ }
+ public void setHidden(int hidden) {
+  this.hidden = hidden;
+ }
+ 
 }
